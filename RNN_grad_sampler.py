88dc97e756ee3e08df1b7bb24006a769f7a6cf3a
rdevon <devon@cs.unm.edu>
2015-06-25 11:25:51 -0700

    Finished grad-based inference Modified MNIST iterator to be able to restrict digits to certain classes.

---
+++ RNN_grad_sampler.py
@@ -0,0 +1,74 @@
+'''
+Sampling and inference with LSTM models
+'''
+
+import numpy as np
+import theano
+from theano import tensor as T
+from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
+import time
+
+from gru import GRU
+from gru import GenStochasticGRU
+from gru import SimpleInferGRU
+from mnist import mnist_iterator
+import op
+from tools import itemlist
+
+floatX = theano.config.floatX
+
+
+def test(batch_size=1, dim_h=100, l=.01, n_inference_steps=100):
+    train = mnist_iterator(batch_size=2*batch_size, mode='train',
+                           restrict_digits=[3, 5, 8])
+    dim_in = train.dim
+
+    X = T.tensor3('x', dtype=floatX)
+
+    trng = RandomStreams(6 * 23 * 2015)
+    rnn = SimpleInferGRU(dim_in, dim_h, trng=trng)
+    tparams = rnn.set_tparams()
+    mask = T.ones_like(X).astype('float32')
+    (x_hats, energies), updates = rnn.inference(
+        X, mask, l, n_inference_steps=n_inference_steps)
+    grads = T.grad(energies[-1], wrt=itemlist(tparams))
+
+    chain, updates_s = rnn.sample(X[0])
+    updates.update(updates_s)
+
+    lr = T.scalar(name='lr')
+    optimizer = 'rmsprop'
+    f_grad_shared, f_grad_updates = eval('op.' + optimizer)(
+        lr, tparams, grads, [X], energies[-1],
+        extra_ups=updates,
+        extra_outs=[x_hats, chain])
+
+    print 'Actually running'
+    learning_rate = 0.001
+    for e in xrange(10000):
+        x, _ = train.next()
+        x = x[:, None, :].astype(floatX)
+        rval = f_grad_shared(x)
+        r = False
+        for k, out in zip(['energy'], rval):
+            if np.any(np.isnan(out)):
+                print k, 'nan'
+                r = True
+            elif np.any(np.isinf(out)):
+                print k, 'inf'
+                r = True
+        if r:
+            return
+        if e % 10 == 0:
+            print e, rval[0], np.exp(-rval[0])
+        if e % 100 == 0:
+            sample = rval[1][:, :, 0]
+            sample[0] = x[:, 0, :]
+            train.save_images(sample, '/Users/devon/tmp/grad_sampler.png')
+            sample_chain = rval[2]
+            train.save_images(sample_chain, '/Users/devon/tmp/grad_chain.png')
+
+        f_grad_updates(learning_rate)
+
+if __name__ == '__main__':
+    test()
